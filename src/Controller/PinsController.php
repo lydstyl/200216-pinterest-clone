00d0496cfdb13d3932a3215929a19087db1d35cc
Gabriel Brun <lydstyl@gmail.com>
2020-02-20 17:24:14 +0100

    refacto with PinRepository injection

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Pin;
-use Doctrine\ORM\EntityManagerInterface;
+// use App\Entity\Pin;
+// use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\PinRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,7 +21,8 @@ class PinsController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index(EntityManagerInterface $em): Response
+    // public function index(EntityManagerInterface $em): Response
+    public function index(PinRepository $repo): Response // injection of PinRepository working in a Controller method otherwise use __construct
     {
         // $pin = new Pin;
         // $pin->setTitle('Title 3');
@@ -35,12 +37,12 @@ class PinsController extends AbstractController
         // // die;
         // // dd($pin);
 
-        dump(Pin::class);
 
-        $repo = $em->getRepository(Pin::class); // Pin::Class is same as "App\Entity\Pin"
+        // $repo = $em->getRepository(Pin::class); // Pin::Class is same as "App\Entity\Pin" // or wit alias in doctrine.yaml "App\Pin"
 
-        $pins = $repo->findAll();
+        // $pins = $repo->findAll();
 
-        return $this->render('pins/index.html.twig', ['pins' => $pins]);
+        // return $this->render('pins/index.html.twig', compact('pins')); // compact('pins') is ['pins' => $pins]
+        return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
     }
 }
