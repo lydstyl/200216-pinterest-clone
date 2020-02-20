e9483489b85d2ea401419a1f45e764c1cade6b01
Gabriel Brun <lydstyl@gmail.com>
2020-02-20 16:20:08 +0100

    injection de dépendance EntityManagerInterface

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,24 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/")
      */
     public function index(): Response
     {
         $pin = new Pin;
-        $pin->setTitle('Title 2');
-        $pin->setDescription('Description 2');
+        $pin->setTitle('Title 3');
+        $pin->setDescription('Description 3');
 
-        $em = $this->getDoctrine()->getManager(); // entity manager
-        $em->persist($pin);
-        $em->flush();
+        //$em = $this->getDoctrine()->getManager(); // entity manager
+        $this->em->persist($pin);
+        $this->em->flush();
 
         // var_dump($pin);
         // dump($pin);
